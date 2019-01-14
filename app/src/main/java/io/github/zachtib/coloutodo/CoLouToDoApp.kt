c18b11ee0cda920c89be098aab47e3cd2ae19e22
Zach Tibbitts <ztibbitts@hchb.com>
2019-01-14 08:48:47 -0500

    Add Koin and Room, migrate more components to AndroidX

--- app/src/main/java/io/github/zachtib/coloutodo/CoLouToDoApp.kt
+++ app/src/main/java/io/github/zachtib/coloutodo/CoLouToDoApp.kt
@@ -1,7 +1,11 @@
 package io.github.zachtib.coloutodo
 
 import android.app.Application
+import org.koin.android.ext.android.startKoin
 
 class CoLouToDoApp : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(this, listOf())
+    }
 }
